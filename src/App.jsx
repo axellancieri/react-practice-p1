1cdbc91bd07a0bdd1ac763e42e4f1c65e89b9467
axellancieri <axel.lavignasse@gmail.com>
2024-12-03 23:43:42 -0300

    made form and now starting to look into tables

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 function App() {
   return (
-    <header id="header">
-      <img src="../public/investment-calculator-logo.png" alt="Investment Calculator" />
-      <h1>Investment Calculator</h1>
-    </header>
+    <>
+      <header id="header">
+        <img src="../public/investment-calculator-logo.png" alt="Investment Calculator" />
+        <h1>Investment Calculator</h1>
+      </header>
+      <form action="" id="user-input">
+
+        <label htmlFor="" className="input-group">Sabalaketum</label>
+        <input type="text" />
+
+        <label htmlFor="" className="input-group">Sabalaketum</label>
+        <input type="text" />
+
+        <label htmlFor="" className="input-group">Sabalaketum</label>
+        <input type="text" />
+
+        <label htmlFor="" className="input-group">Sabalaketum</label>
+        <input type="text" />
+
+      </form>
+
+      
+    </>
   )
 }
 
