ef93a57130705824f9c926d84ccb6ca227dd58b2
axellancieri <axel.lavignasse@gmail.com>
2024-12-08 19:49:11 -0300

    got table looking decent, will now go start making components of everything

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
         <img src="../public/investment-calculator-logo.png" alt="Investment Calculator" />
         <h1>Investment Calculator</h1>
       </header>
-      <form action="" id="user-input">
+      <form action="" id="user-input" className="center">
 
         <label htmlFor="" className="input-group">Sabalaketum</label>
         <input type="text" />
@@ -20,7 +20,35 @@ function App() {
         <input type="text" />
 
       </form>
-<table></table>
+
+<table id="result">
+  <thead className="center">
+    <tr>
+      <th>
+        Year
+      </th>
+      <th>
+        Investment Value
+      </th>
+      <th>
+        Interest (Year)
+      </th>
+      <th>
+        Total Interest
+      </th>
+      <th>
+        Invested Capital
+      </th>
+    </tr>
+  </thead>
+  <tbody className="center">
+    <tr>
+      <td>
+        Trabaja
+      </td>
+    </tr>
+  </tbody>
+</table>
       
     </>
   )
